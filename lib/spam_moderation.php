35020cc06a464cfadc63f514fdf073cf3b44ae96
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2011-04-14 05:15:43 +0530

    Removed the word_list.txt. Now admins add their own word list in the plugin settings page

--- lib/spam_moderation.php
+++ lib/spam_moderation.php
@@ -1,13 +1,25 @@
 <?php
 /**
- * 	SPAM Moderation
+ *   Copyright SpamModeration Plugin Ashwanth Kumar <ashwanthkumar@googlemail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+ /**
  * 	This service plugin allows the system to validate the content posted on the community against Akismet spam service.
  *
  *	@package SpamModeration
- *
- *	@date 13/04/2011 - LAst Updated
- *	@author Ashwanth Kumar <ashwanthkumar@googlemail.com>
- *	@license Apache License Version 2
+ *	@date 14/04/2011 - Last Updated
  */
 
 /**
--- start.php
+++ start.php
@@ -1,12 +1,25 @@
 <?php
 /**
- * SPAM Moderation
- * This service plugin allows the system to validate the content posted on the community against Akismet spam service.
+ *   Copyright SpamModeration Plugin Ashwanth Kumar <ashwanthkumar@googlemail.com>
  *
- * @package SpamModeration
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *	@date 13/04/2011 - LAst Updated
- *	@author Ashwanth Kumar <ashwanthkumar@googlemail.com>
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+ /**
+ * 	This service plugin allows the system to validate the content posted on the community against Akismet spam service.
+ *
+ *	@package SpamModeration
+ *	@date 14/04/2011 - Last Updated
  */
 
 elgg_register_event_handler('init', 'system', 'spam_moderation_init');
